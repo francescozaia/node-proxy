207beb91f03ba830caa8d07db743967de6b56384
Francesco Zaia <francescozaia@MacBook-Air.homenet.telecomitalia.it>
2016-10-25 09:57:38 +0200

    Port change

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 require('./routes')(app);
-app.listen(3001);
-console.log('Listening on port 3001...');
+app.listen(80);
+console.log('Listening on port 80...');
